fab1c398424cd6597ade0005a36a47a7f487e9aa
Bhargav Mule <bhargavmule16@gmail.com>
2025-12-15 22:52:21 +0530

    config fixes

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
+import dotenv from "dotenv"
+
+dotenv.config()
+
 export const BUCKET_NAME = "pipeline_oneminus"
 
 export const CLOUD_CORS_RESPONSE_HEADERS = ["Content-Type","x-goog-meta-*","x-goog-storage-class","Content-Disposition"]
@@ -5,11 +9,12 @@ export const CLOUD_CORS_METHODS = ["GET","HEAD","OPTIONS","PUT"]
 
 
 
+
 export const LOCAL_ENV_ORIGIN = "*"
-export const DEV_ENV_ORIGIN = ""
+export const DEV_ENV_ORIGIN = process.env.DEV_ENV_ORIGIN
 export const PROD_ENV_ORIGIN = ""
 
 export const LOCAL_ENV = "local"
 export const DEV_ENV = "dev"
 export const PROD_ENV = "prod"
-export const ENVIRONMENT = "local" 
+export const ENVIRONMENT = process.env.ENV 
