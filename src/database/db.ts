c6c6cbfeeb497c153ca5242d92ef8f9972419924
Bhargav Mule <bhargavmule16@gmail.com>
2026-08-02 22:18:36 +0530

    refactor: index.ts and db.ts

---
+++ src/database/db.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+//constants
+const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_NAME = process.env.DATABASE_NAME;
+
+const connectToDatabase = async () => {
+	try {
+		const res = await mongoose.connect(`${DATABASE_URL}/${DATABASE_NAME}`);
+		console.log(`Database sucessfully connected : `, res.connection.host);
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
+};
+
+export default connectToDatabase;
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,26 @@
-import app, { socketApp } from "./app"
-import { connectToDatabase } from "./db/db"
-import dotenv from "dotenv"
-import { exit } from "process"
-import { startupSetup } from "./services/startup.setup"
+import app, { socketApp } from "./app";
+import { exit } from "process";
+import { startupSetup } from "./services/startup.setup";
+import connectToDatabase from "./database/db";
 
-
-dotenv.config()
-
-const PORT = process.env.PORT;
+//constants
+const APPLICATION_PORT = process.env.APPLICATION_PORT;
 const WEBSOCKET_PORT = process.env.WEBSOCKET_PORT;
 
 connectToDatabase()
-.then(()=>{
+	.then(() => {
+		app.listen(APPLICATION_PORT, () => {
+			console.log(`Server is up and running on PORT : ${APPLICATION_PORT}`);
+		});
 
-  socketApp.listen(WEBSOCKET_PORT, ()=>{
-    console.log(`Socket Server is up and Running on PORT : ${WEBSOCKET_PORT}`)
-  })
+		socketApp.listen(WEBSOCKET_PORT, () => {
+			console.log(`Socket Server is up and Running on PORT : ${WEBSOCKET_PORT}`);
+		});
 
-  app.listen(PORT,()=>{
-    console.log(`Server is up and running on PORT : ${PORT}`)
-  })
-
-  //Perform Setup
-  startupSetup()
-
-})
-.catch((err)=>{
-    console.log(`Some unexpected error has occured while connecting to DB : ${err}`)
-    exit(1)
-})
-
+		//Perform Setup
+		startupSetup();
+	})
+	.catch((err) => {
+		console.log(`Some unexpected error has occured while connecting to DB : ${err}`);
+		exit(1);
+	});
